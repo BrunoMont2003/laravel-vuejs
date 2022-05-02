c5ea39a56ddc3b71484478c138230b0fb76d3a94
brunomont <brunoadrian.mont@gmail.com>
2022-05-01 19:31:02 -0500

    add: begin with form components, tabs etc

---
+++ resources/js/components/resume/ResumeForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Tabs>
+      <Tab title="Basics" icon="user">
+        <VueFormGenerator
+          :schema="schemas.basics"
+          :model="resume.content.basics"
+          :options="options"
+        />
+      </Tab>
+      <Tab title="Work" icon="briefcase"></Tab>
+      <Tab title="Education" icon="building"></Tab>
+      <Tab title="Awards" icon="award"></Tab>
+      <Tab title="Skills" icon="bolt"></Tab>
+    </Tabs>
+  </div>
+</template>
+
+<script>
+import Tabs from "./tabs/Tabs";
+import Tab from "./tabs/Tab";
+import { component as VueFormGenerator } from "vue-form-generator";
+import "vue-form-generator/dist/vfg.css";
+import basics from "./schema/basics/basics";
+export default {
+  name: "ResumeForm",
+  components: {
+    Tabs,
+    Tab,
+    VueFormGenerator,
+  },
+  data() {
+    return {
+      resume: {
+        title: "",
+        content: {
+          basics: {},
+        },
+      },
+      schemas: {
+        basics: basics,
+      },
+      options: {
+        validateAfterLoad: true,
+        validateAfterChanged: true,
+        validateAsync: true,
+      },
+    };
+  },
+};
+</script>
+<style>
+.vue-form-generator {
+  margin-top: 1rem;
+}
+.form-control {
+  background-color: transparent !important;
+  transition: all 0.3s ease-in-out !important;
+}
+.form-control:focus {
+  background-color: rgb(255, 255, 255) !important;
+  box-shadow: rgba(13, 110, 253, 0.3) 0px 0px 0px 3px;
+}
+</style>
---
+++ resources/js/components/resume/tabs/Tab.vue
@@ -0,0 +1,35 @@
+<template>
+  <BTab title-item-class="p-2">
+    <template #title>
+      <div class="d-flex justify-content-center align-items-center p-2">
+        <font-awesome-icon
+          :icon="icon"
+          style="font-size: 20px; margin-right: 1rem"
+        ></font-awesome-icon>
+        <span class="d-none d-md-block">
+          {{ title }}
+        </span>
+      </div>
+    </template>
+    <slot></slot>
+  </BTab>
+</template>
+
+<script>
+import { BTab } from "bootstrap-vue";
+export default {
+  name: "Tab",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+    },
+  },
+  components: {
+    BTab,
+  },
+};
+</script>
