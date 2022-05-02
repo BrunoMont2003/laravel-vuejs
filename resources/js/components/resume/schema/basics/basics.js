5291030462120f63594adcfc4d430939309a74bd
brunomont <brunoadrian.mont@gmail.com>
2022-05-01 19:32:40 -0500

    add: the schema of basics

---
+++ resources/js/components/resume/schema/basics/basics.js
@@ -0,0 +1,60 @@
+export default {
+  fields: [
+    // Name
+    {
+      type: "input",
+      inputType: "text",
+      placeholder: "Your name",
+      label: "Name",
+      model: "name",
+      styleClasses: ["col-12", "col-md-6", "px-1"],
+    },
+    // Label
+    {
+      type: "input",
+      inputType: "text",
+      placeholder: "Programmer",
+      label: "Label",
+      model: "label",
+      styleClasses: ["col-12", "col-md-6", "px-1"],
+    },
+    // Email
+    {
+      type: "input",
+      inputType: "email",
+      placeholder: "john@gmail.com",
+      label: "Email",
+      model: "email",
+      validator: "email",
+      styleClasses: ["col-12", "col-md-4", "px-1"],
+    },
+    // Phone
+    {
+      type: "input",
+      inputType: "tel",
+      placeholder: "987654321",
+      label: "Phone",
+      model: "Phone",
+      styleClasses: ["col-12", "col-md-4", "px-1"],
+    },
+    // website
+    {
+      type: "input",
+      inputType: "text",
+      placeholder: "https://johndoe.com",
+      label: "Website",
+      model: "website",
+      validator: "url",
+      styleClasses: ["col-12", "col-md-4", "px-1"],
+    },
+    // Summary
+    {
+      type: "textArea",
+      inputType: "text",
+      placeholder: "A little bit about yourself",
+      label: "Summary",
+      model: "summary",
+      styleClasses: ["px-1"],
+    },
+  ],
+};
