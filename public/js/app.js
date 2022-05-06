aaeaba705e17678450bcf8acc4f671747e09f9bb
brunomont <brunoadrian.mont@gmail.com>
2022-05-06 16:10:02 -0500

    add : list resumes nav link

--- public/js/app.js
+++ public/js/app.js
@@ -20269,9 +20269,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _reusable_Alert__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ../reusable/Alert */ "./resources/js/components/reusable/Alert.vue");
 
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 //
 //
@@ -20465,63 +20481,144 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     };
   },
   methods: {
+    validate: function validate(target) {
+      var parent = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "resume";
+      var errors = [];
+
+      for (var _i = 0, _Object$entries = Object.entries(target); _i < _Object$entries.length; _i++) {
+        var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+            prop = _Object$entries$_i[0],
+            value = _Object$entries$_i[1];
+
+        if (Array.isArray(value)) {
+          if (value.length === 0) {
+            errors.push("".concat(parent, " > ").concat(prop, " must have at least one element"));
+            continue;
+          }
+
+          for (var i in value) {
+            if (typeof value[i] === null || value[i] === "") {
+              errors.push("".concat(parent, " > ").concat(prop, " > ").concat(i, " cannot be empty"));
+            } else if (_typeof(value[i]) === "object") {
+              errors = errors.concat(this.validate(value[i], "".concat(parent, " > ").concat(prop, " > ").concat(i)));
+            }
+          }
+        } else if (_typeof(value) === "object") {
+          errors = errors.concat(this.validate(value, "".concat(parent, " > ").concat(prop)));
+        } else if (value === null || value === "") {
+          errors.push("".concat(parent, " > ").concat(prop, " is required"));
+        }
+      }
+
+      return errors;
+    },
+    isValid: function isValid() {
+      var alert = this.$data.alert;
+      var resume = this.$props.resume;
+      alert.messages = [];
+      var errors = this.validate(resume.content);
+
+      if (errors.length < 1) {
+        return true;
+      }
+
+      alert.messages = errors.slice(0, 3);
+
+      if (errors.length > 3) {
+        alert.messages.push("<strong>".concat(errors.length - 3, " more errors ...</strong>"));
+      }
+
+      alert.type = "warning";
+      return false;
+    },
     submit: function submit() {
       var _this = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
-        var _ref, data, status;
+        var alert, _this$$props, resume, update, _ref, data, status, errors, _i2, _Object$entries2, _Object$entries2$_i, prop, value, origin, _iterator, _step, error, message;
 
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                _context.prev = 0;
+                alert = _this.$data.alert;
+                _this$$props = _this.$props, resume = _this$$props.resume, update = _this$$props.update;
 
-                if (!_this.update) {
-                  _context.next = 7;
+                if (_this.isValid()) {
+                  _context.next = 4;
                   break;
                 }
 
-                _context.next = 4;
-                return axios__WEBPACK_IMPORTED_MODULE_15___default().put("/resumes/" + _this.resume.id, _this.resume);
+                return _context.abrupt("return");
 
               case 4:
+                _context.prev = 4;
+
+                if (!update) {
+                  _context.next = 11;
+                  break;
+                }
+
+                _context.next = 8;
+                return axios__WEBPACK_IMPORTED_MODULE_15___default().put("/resumes/" + resume.id, resume);
+
+              case 8:
                 _context.t0 = _context.sent;
-                _context.next = 10;
+                _context.next = 14;
                 break;
 
-              case 7:
-                _context.next = 9;
-                return axios__WEBPACK_IMPORTED_MODULE_15___default().post("/resumes", _this.resume);
+              case 11:
+                _context.next = 13;
+                return axios__WEBPACK_IMPORTED_MODULE_15___default().post("/resumes", resume);
 
-              case 9:
+              case 13:
                 _context.t0 = _context.sent;
 
-              case 10:
+              case 14:
                 _ref = _context.t0;
                 data = _ref.data;
                 status = _ref.status;
                 console.log(data);
 
                 if (status >= 200 && status < 300) {
-                  window.location = "/home";
+                  window.location = "/resumes";
                 }
 
-                _context.next = 21;
+                _context.next = 27;
                 break;
 
-              case 17:
-                _context.prev = 17;
-                _context.t1 = _context["catch"](0);
-                console.log(_context.t1.message);
-                _this.alert.messages = ["Something went wrong: "];
-
               case 21:
+                _context.prev = 21;
+                _context.t1 = _context["catch"](4);
+                alert.messages = [];
+                errors = _context.t1.response.data.errors;
+
+                for (_i2 = 0, _Object$entries2 = Object.entries(errors); _i2 < _Object$entries2.length; _i2++) {
+                  _Object$entries2$_i = _slicedToArray(_Object$entries2[_i2], 2), prop = _Object$entries2$_i[0], value = _Object$entries2$_i[1];
+                  origin = prop.split(".").join(" > ");
+                  _iterator = _createForOfIteratorHelper(value);
+
+                  try {
+                    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+                      error = _step.value;
+                      message = error.replace(prop, "<strong>".concat(origin, "</strong>"));
+                      alert.messages.push(message);
+                    }
+                  } catch (err) {
+                    _iterator.e(err);
+                  } finally {
+                    _iterator.f();
+                  }
+                }
+
+                alert.type = "danger";
+
+              case 27:
               case "end":
                 return _context.stop();
             }
           }
-        }, _callee, null, [[0, 17]]);
+        }, _callee, null, [[4, 21]]);
       }))();
     }
   }
@@ -21311,7 +21408,8 @@ __webpack_require__.r(__webpack_exports__);
     placeholder: "https://twitter.com/jhondoe",
     label: "Url",
     model: "url",
-    styleClasses: ["col-12", "col-md-4", "px-1"]
+    styleClasses: ["col-12", "col-md-4", "px-1"],
+    validator: "url"
   }, {
     type: "input",
     inputType: "text",
@@ -21457,7 +21555,8 @@ __webpack_require__.r(__webpack_exports__);
     placeholder: "http://www.website.com",
     label: "Website",
     model: "website",
-    styleClasses: ["col-12", "col-md-4", "px-1"]
+    styleClasses: ["col-12", "col-md-4", "px-1"],
+    validator: "url"
   }, // startDate
   {
     type: "input",
